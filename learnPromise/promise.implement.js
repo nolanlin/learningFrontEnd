c24ce13cba5f8f1e3f6e7e1b747021ec046ef23d
Nolan <n1023456@163.com>
2021-03-09 16:31:40 +0800

    commit third times

--- learnPromise/promise.implement.js
+++ learnPromise/promise.implement.js
@@ -1,24 +1,28 @@
 /**
- * 依照Promises/A+规范 https://promisesaplus.com/#point-9
+ * 依照Promises/A+规范 https://promisesaplus.com/
  * 思考：
  * 1. 如何用？
  * 2. 定义Terminology
  *  2.1. state: pending, fulfilled, rejected
- * 3. 思考resolve、then的执行过程
+ * 3. 思考resolve、reject、then的执行过程
+ *  3.1. executor是同步执行，then是微任务，用setTimeout来模拟then的微任务队列行为
+ *  3.2. then是一个收集onFullfilled和onRejected回调方法的过程
+ *  3.3. resolve和reject应当在then注册之后执行收集到的回调函数
  * 4. 参照规范，条件输出
  */
-class CustomPromise{
-  constructor(handleFunc){
+class CustomPromise {
+  constructor (handleFunc) {
     this.state = 'pending';
     this.value = undefined;
     this.exception = undefined;
     this.reason = undefined;
 
     this.fulfilledList = [];
-    handleFunc(this.triggerOnFulfilled.bind(this));
+    this.rejectedList = [];
+    handleFunc(this.triggerResolve.bind(this));
   }
-  triggerOnFulfilled(val) {
-    if(this.state !== 'pending') return;
+  triggerResolve(val) {
+    if (this.state !== 'pending') return;
     setTimeout(() => {
       this.state = 'fulfilled';
       this.value = val;
@@ -26,20 +30,62 @@ class CustomPromise{
       this.fulfilledList = [];
     }, 0);
   }
+  then(onFulfilled, onRejected){
+    const { state, value, reason } = this
+    return new CustomPromise((nextResolve, nextReject) => {
+      function onFinalFulfilled(val){
+        if(typeof onFulfilled !== 'function'){
+          nextResolve(val);
+        }else{
+          const res = onFulfilled(val);
+          if(res && typeof res.then === 'function'){
+            res.then(nextResolve);
+          }else{
+            nextResolve(res);
+          }
+        }
+      }
+      switch(state){
+        case 'pending': {
+          this.fulfilledList.push(onFinalFulfilled);
+          break;
+        }
+      }
+    })
+  }
 }
 
-const createPromise = function(number){
-  return new CustomPromise((onFulfilled, onReject)=>{
-    if(number > 1000){
-      onFulfilled(number);
-    }else{
-      onReject(number);
+const createPromise = function (number) {
+  return new CustomPromise((resolve, reject) => {
+    if (number > 1000) {
+      resolve(number);
+    } else {
+      reject(number);
     }
   });
 }
 const promiseInstance = createPromise(1200);
-promiseInstance.catch(function(error){
-  console.error('reject promise', error);
-}).then(function(number){
-  console.log('fulfilled promise', number);
+promiseInstance.then(function (number) {
+  console.log('fulfilled promise 1', number);
+  return number;
+}).then(function (number) {
+  console.log('fulfilled promise 2', number);
+  return number;
+}).then(function (number) {
+  console.log('fulfilled promise 3', number);
+  return number;
 })
+promiseInstance.then(function (number) {
+  console.log('fulfilled promise 4', number);
+  return number;
+}).then(function (number) {
+  console.log('fulfilled promise 5', number);
+  return number;
+})
+// ....
+setTimeout(() => {
+  promiseInstance.then(function (number) {
+    console.log('fulfilled promise 6', number);
+    return number;
+  })
+}, 0);
