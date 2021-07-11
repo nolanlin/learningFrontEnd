d117752f0379bbc9b65ecab91f0ef276e3a2b720
Nolan <n1023456@163.com>
2021-07-11 17:30:18 +0800

    2021071102

--- learnBrowser/README.md
+++ learnBrowser/README.md
@@ -1,1 +1,5 @@
 # Browser - 路白
+
+## BOM - Browser Object Model
+
+- 浏览器提供的 API，比如 window, location, navigator, screen, history
--- learnThis/learnThis.js
+++ learnThis/learnThis.js
@@ -10,3 +10,62 @@ function objectCreate() {
 }
 const whitePlayer = objectCreate(Player, "white");
 console.log(whitePlayer);
+
+// 练习题
+// 1.
+function foo() {
+  console.log(this.a); // console what
+}
+var a = 2;
+(function () {
+  "use strict"; // 迷惑大家的
+  foo();
+})();
+
+// 2.
+var name = "the window";
+
+var object = {
+  name: "My Object",
+  getName: function () {
+    return this.name;
+  },
+};
+object.getName(); // console what ?
+object.getName(); // console what ?
+(object.getName = object.getName)(); // console what ?
+(object.getName, object.getName)(); // console what ?
+
+// 3.
+var x = 3;
+var obj3 = {
+  x: 1,
+  getX: function () {
+    var x = 5;
+    return (function () {
+      return this.x;
+    })(); // ⚠️
+  },
+};
+console.log(obj3.getX()); // console what?
+
+// 4.
+function a(x) {
+  this.x = x;
+  return this;
+}
+var x = a(5); // 替换为 let 再试试
+var y = a(6); // 替换为 let 再试试 // 再换回 var，但是去掉 y 的情况，再试试
+
+console.log(x.x); // console what ?
+console.log(y.x); // console what ?
+
+// 等价于
+window.x = 5;
+window.x = window;
+
+window.x = 6;
+window.y = window;
+
+console.log(x.x); // void 0 其实执行的是 Number(6).x
+console.log(y.x); // 6
