ca65cd705372c8229f4fd3e92f673f3d34478ddd
Nolan <n1023456@163.com>
2021-11-05 17:04:55 +0800

    2021110502

--- learnWebpack/src/react.js
+++ learnWebpack/src/react.js
@@ -2,28 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import "./style.css";
 
-const lazy = (fn) =>
-  class extends React.Component {
-    state = {
-      Component: () => null,
-    };
-
-    async componentDidMount() {
-      const { default: Component } = await fn();
-      this.setState({ Component });
-    }
-
-    render() {
-      const Component = this.state.Component;
-      return <Component {...this.props} />;
-    }
-  };
-
-const Async = lazy(() => import("./Async"));
-
-// 指定产出的模块名称（注意这里的注释是有用的）：
-const Async = lazy(() => import(/* webpackChunkName: "Async" */ "./Async"));
-
 const App = () => <div>App</div>;
 
 render(<App />, document.querySelector("#app"));
